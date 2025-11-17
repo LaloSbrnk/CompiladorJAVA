6db4812c967345b15d28e4996bcbd11fb394715f
Ladislao <135392331+LaloSbrnk@users.noreply.github.com>
2025-11-17 14:54:08 -0300

    Delete Compiladores-Grupo11-Arispe-Pianzola-Sabarnik directory

--- Compiladores-Grupo11-Arispe-Pianzola-Sabarnik/conjuntoSimbolos/ConjuntoL.java
+++
@@ -1,8 +0,0 @@
-package conjuntoSimbolos;
-
-public class ConjuntoL extends ConjuntoSimbolos {
-    @Override
-    public boolean contieneSimbolo(char simbolo) {
-        return simbolo == 'L';
-    }
-}
