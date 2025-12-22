b23801ef3cdb7a2c034d68c7308490159920e1e6
Ladislao <135392331+LaloSbrnk@users.noreply.github.com>
2025-12-22 05:01:49 -0300

    Delete Compiladores-Grupo11-Arispe-Pianzola-Sabarnik directory

--- Compiladores-Grupo11-Arispe-Pianzola-Sabarnik/conjuntoSimbolos/ConjuntoDosPuntos.java
+++
@@ -1,8 +0,0 @@
-package conjuntoSimbolos;
-
-public class ConjuntoDosPuntos extends ConjuntoSimbolos{
-    @Override
-    public boolean contieneSimbolo(char simbolo) {
-        return (simbolo == ':');
-    }
-}
--- Compiladores-Grupo11-Arispe-Pianzola-Sabarnik/conjuntoSimbolos/ConjuntoMinus.java
+++
@@ -1,8 +0,0 @@
-package conjuntoSimbolos;
-
-public class ConjuntoMinus extends ConjuntoSimbolos{
-    @Override
-    public boolean contieneSimbolo(char simbolo) {
-        return Character.isLowerCase(simbolo);
-    }
-}
